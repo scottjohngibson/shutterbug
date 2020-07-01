ed5f026fa86462ee9cc5cf1d39bd13fb325868fa
scottjohngibson <srb19157@uni.strath.ac.uk>
2020-07-01 14:10:56 +0100

    Unit Testing
    - Set up unit testing framework
    - Finished unit testing index.html
    - Removed seperate codebase folders

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let ChooseGenre = {
+
+
+	selectPortrait: function(){
+
+	let img = document.getElementById("portrait");
+	let nextPage = document.getElementById("continue");
+
+	img.src="../img/portraitOn.png";
+	nextPage.disabled = "false"
+
+	}
+
+}
+
+
+module.exports = ChooseGenre
+
+
+
+
+
